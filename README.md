89151344dbf359825cda2a0ad10fee59567a6562
Roberta Drayer <robertadrayer@gmail.com>
2023-10-15 13:03:17 -0700

    finished db models, added proj desc in readme

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 API: https://developers.strava.com/
 DBSchema: https://app.quickdatabasediagrams.com/#/d/E6VDZp
+
+App in a nutshell: users will have the ability to create an account and create + display each of their activites (pretty similiar to Strava's existing functionality on activity viewing + sharing). Home page will display activities from all users, sorted by most recent activity. 
+Maybes: 
+    -ability to like/comment on activities
+    -stats per athlete. 
--- models.py
+++ models.py
@@ -12,7 +12,7 @@ bcrypt = Bcrypt()
 class User(db.Model):
     __tablename__ = 'users'
     '''Users'''
-    id = db.Column(db.Integer,
+    user_id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
     username = db.Column(db.String(20),
@@ -24,3 +24,46 @@ class User(db.Model):
                            nullable=False)
     last_name = db.Column(db.String(30),
                         nullable=False)
+
+class Activity(db.Model):
+    __tablename__ = 'activites'
+    '''Activities'''
+    activity_id = db.Column(db.Integer,
+                            primary_key=True,
+                            autoincrement=True)
+    name = db.Column(db.String,
+                     nullable=False)
+    type = db.Column(db.String,
+                     nullable=False)
+    distance = db.Column(db.Integer,
+                         nullable=False)
+    date = db.Column(db.Date,
+                     nullable=False)
+    time = db.column(db.Time,
+                         nullable=False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id', ondelete='cascade'))
+    
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    '''Comments'''
+    comment_id = db.Column(db.Integer,
+                           primary_key=True,
+                           autoincrement=True)
+    comment = db.Column(db.String,
+                        nullable=False)
+    user_id = db.Column(db.Integer,
+                db.ForeignKey('users.user_id', ondelete='cascade'))
+    activity_id = db.Column(db.Integer,
+                            db.ForeignKey('activities.activity_id', ondelete='cascade'))
+    
+class Likes(db.Model):
+    __tablename__ = 'likes'
+    '''Likes'''
+    like_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True)
+    user_id = db.Column(db.Integer,
+                    db.ForeignKey('users.user_id', ondelete='cascade'))
+    activity_id = db.Column(db.Integer,
+                        db.ForeignKey('activities.activity_id', ondelete='cascade'))
